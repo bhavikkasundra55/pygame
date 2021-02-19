5a9f21059d20c79c66793a2dbc469505168a5234
Bhavik kasundra <u19me151@med.svnit.ac.in>
2021-02-19 23:36:19 +0530

    enemy step down mechanics

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import pygame
-#Initialize pygame
+import random
+
+# Initialize pygame
 pygame.init()
+
 # Create Game screen
-screen = pygame.display.set_mode((800,600))
+screen = pygame.display.set_mode((800, 600))
 
 pygame.display.set_caption('Firing mortals')
 icon = pygame.image.load('icon.png')
@@ -11,17 +14,23 @@ pygame.display.set_icon(icon)
 playerimg = pygame.image.load('player.png')
 playerX = 400
 playerY = 480
+playerX_change = 0
 
 enemyimg = pygame.image.load('enemy.png')
-enemyX = 360
-enemyY = 80
+enemyX = random.randint(0, 736)
+enemyY = random.randint(32, 250)
+enemyX_change = 0.3
+enemyY_change = 50
 
-def player(x,y):
-    screen.blit(playerimg,(x,y))
-def enemy(x,y):
-    screen.blit(enemyimg,(x,y))
-playerX_change = 0
-#Create game loop
+def player(x, y):
+    screen.blit(playerimg, (x, y))
+
+
+def enemy(x, y):
+    screen.blit(enemyimg, (x, y))
+
+
+# Create game loop
 running = True
 while running:
 
@@ -41,11 +50,20 @@ while running:
 
     playerX += playerX_change
 
-    if playerX <= 0 :
+    if playerX <= 0:
         playerX = 0
-    elif playerX >=760:
+    elif playerX >= 760:
         playerX = 760
 
-    enemy(enemyX,enemyY)
-    player(playerX,playerY)
+    enemyX += enemyX_change
+
+    if enemyX <= 0:
+        enemyX_change = 0.3
+        enemyY += enemyY_change
+    elif enemyX >= 740:
+        enemyX_change = -0.3
+        enemyY += enemyY_change
+
+    player(playerX, playerY)
+    enemy(enemyX, enemyY)
     pygame.display.update()
