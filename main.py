a84f5df17d608ca18b424418e352e89909cf8b78
Bhavik kasundra <u19me151@med.svnit.ac.in>
2021-02-19 22:01:41 +0530

    Initial commit

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import pygame
-
 #Initialize pygame
 pygame.init()
-
 # Create Game screen
-screen = pygame.display.set_mode((720,720))
+screen = pygame.display.set_mode((800,600))
+
+pygame.display.set_caption('Firing mortals')
+icon = pygame.image.load('icon.png')
+pygame.display.set_icon(icon)
+
+playerimg = pygame.image.load('player.png')
+playerX = 400
+playerY = 480
+
+def player(x,y):
+    screen.blit(playerimg,(x,y))
 
 #Create game loop
 running = True
@@ -12,3 +21,7 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    screen.fill((0,0,0))
+    player()
+    pygame.display.update()
