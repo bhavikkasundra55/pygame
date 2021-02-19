a994c38b4fb49d2ded51823f6c96babc9645a8d8
Bhavik kasundra <u19me151@med.svnit.ac.in>
2021-02-19 23:08:29 +0530

    enemy

--- main.py
+++ main.py
@@ -12,21 +12,40 @@ playerimg = pygame.image.load('player.png')
 playerX = 400
 playerY = 480
 
+enemyimg = pygame.image.load('enemy.png')
+enemyX = 360
+enemyY = 80
+
 def player(x,y):
     screen.blit(playerimg,(x,y))
-
+def enemy(x,y):
+    screen.blit(enemyimg,(x,y))
+playerX_change = 0
 #Create game loop
 running = True
 while running:
+
+    screen.fill((0, 0, 0))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.KEYDOWN:
-            if event.type == pygame.K_LEFT:
-                playerX_change = 0.1
-            if event.type == pygame.K_RIGHT:
-                playerX_change = 0.1
+            if event.key == pygame.K_LEFT:
+                playerX_change = -0.3
+            if event.key == pygame.K_RIGHT:
+                playerX_change = 0.3
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_RIGHT or event.key == pygame.K_LEFT:
+                playerX_change = 0
 
-    screen.fill((0,0,0))
-    player()
+    playerX += playerX_change
+
+    if playerX <= 0 :
+        playerX = 0
+    elif playerX >=760:
+        playerX = 760
+
+    enemy(enemyX,enemyY)
+    player(playerX,playerY)
     pygame.display.update()
